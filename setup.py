b3c1d993f71a4ca848126fe068c51bd701d67ea5
Greger Stolt Nilsen <gregersn@gmail.com>
2021-07-28 09:07:04 +0200

    WIP

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="Victor",
+    description="Tool for generating character stats for TTRPGs",
     version="0.1.0",
+    author="Greger Stolt Nilsen",
+    author_email="gregersn@gmail.com",
     packages=find_packages(),
     include_package_data=True,
+    url="https://github.com/gregersn/Victor",
     install_requires=[
         'Click',
         'pikepdf'
