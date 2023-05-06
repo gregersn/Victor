23d49f22c96a78bb2d6a69e0346d3738b6366e51
Greger Stolt Nilsen <gregersn@gmail.com>
2023-05-06 23:32:39 +0200

    Use Trill and Ringneck.
    Remove outdated tests.
    Also add example.

--- victor/__init__.py
+++ victor/__init__.py
@@ -1,31 +1,29 @@
-from typing import Union
+from re import A
+from typing import Any, Dict, Union
 from pathlib import Path
+from victor.interpreter import get_interpreter
 
 from victor.reader import load_rules
-from victor.interpreter import Tokenizer, Parser, Interpreter
 
 from victor.render import fill_pdf
 from victor.character import Character
 
 
-def generate(filename: Union[Path, str], average: bool = False) -> Character:
+def generate(filename: Union[Path, str], average: bool = False) -> Dict[str, Any]:
     if isinstance(filename, str):
         filename = Path(filename)
 
     if not filename.is_file():
         raise FileNotFoundError()
 
-    _, metadata, content = load_rules(filename)
+    with open(filename, 'r', encoding="utf8") as f:
+        program = f.read()
 
-    tokenizer = Tokenizer(metadata)
-    parser = Parser(tokenizer)
-    interpreter = Interpreter(parser)
-    _ = interpreter.interpret(average=average)
+    state: Dict[str, Any] = {}
+    interpreter = get_interpreter(
+        program, state, basedir=filename.absolute().parent)
 
-    char = Character()
-    char.variables = interpreter.variables
-    char.content = content
-    return char
+    return state
 
 
 def fill_sheet(filename: Union[Path, str],
--- victor/interpreter/__init__.py
+++ victor/interpreter/__init__.py
@@ -1,18 +1,1 @@
-from typing import Any, Optional, Dict
-from .tokenizer import Tokenizer
-from .parser import Parser
-from .interpreter import Interpreter
-
-
-def interpret(program: str, **kwargs: Any):
-    interpreter = get_interpreter(program)
-    return interpreter.interpret(**kwargs)
-
-
-def get_interpreter(program: str = "", variables: Optional[Dict[str, Any]] = None):
-    tokenizer = Tokenizer(program)
-    parser = Parser(tokenizer)
-    interpreter = Interpreter(parser)
-    if variables is not None:
-        interpreter.variables = variables
-    return interpreter
+from .generator import get_interpreter  # noqa: F401
